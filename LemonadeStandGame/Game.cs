433f7de5dd9369fe603835bacb301185b6f7ca5d
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-06-25 15:00:29 -0500

    Created the classes currently required.

---
+++ LemonadeStandGame/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandGame
+{
+    class Game
+    {
+        public UserInterface userInterface;
+        public Game()
+        {
+            
+        }
+        public void ExecuteGame()
+        {
+            userInterface = new UserInterface();
+            userInterface.DisplayWelcome();
+            userInterface.DisplayRules();
+        }
+
+
+
+    }
+}
---
+++ LemonadeStandGame/UserInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandGame
+{
+    class UserInterface
+    {
+        public UserInterface()
+        {
+
+        }
+
+        public void DisplayWelcome()
+        {
+            Console.WriteLine("\tWelcome to the Lemonade Stand Game!");
+        }
+
+        public void MakeListOfRules()
+        {
+        }
+
+        public void DisplayRules()
+        {
+            Console.WriteLine("\nThe game rules:");
+        }
+    }
+}
